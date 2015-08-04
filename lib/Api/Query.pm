1abf4b8dabfc9c0fe85ceee4e9f865f2c069192b
Daniel Alves <daniel@danielalves.me>
2015-08-04 23:10:40 +0100

    Add columns and order to query builder

--- lib/Api/Query.pm
+++ lib/Api/Query.pm
@@ -5,26 +5,46 @@ use warnings;
 sub build {
   my ($self, $table_name, $options) = @_;
   my %options = %$options;
-  my @wheres = ();
+  my @where = ();
   my $query_string = "SELECT * FROM $table_name";
+  my $order = "";
+  my @columns = ();
 
   for my $key (sort keys %options) {
+    my $where = $options{$key};
+
+    if ($key eq "columns") {
+      @columns = @{$where};
+    }
+
     if ($key eq "where") {
-      my $where = $options{$key};
-      parse_wheres(\@wheres, $where);
+      parse_where(\@where, $where);
+    }
+
+    if ($key eq "order_by") {
+      parse_order(\$order, $where);
     }
   }
 
-  if (@wheres) {
-    my $joined_wheres = join(" AND ", @wheres);
-    $query_string = $query_string." WHERE $joined_wheres";
+  if (@columns) {
+    my $columns = join(", ", @columns) if (@columns);
+    $query_string = "SELECT $columns FROM $table_name";
+  }
+
+  if (@where) {
+    my $joined_where = join(" AND ", @where);
+    $query_string = $query_string." WHERE $joined_where";
+  }
+
+  if (length $order > 0) {
+    $query_string = $query_string." $order";
   }
 
   $query_string;
 }
 
-sub parse_wheres {
-  my $wheres = shift @_;
+sub parse_where {
+  my $where_ref = shift @_;
   my %where_options = %{shift @_};
 
   for my $key (sort keys %where_options) {
@@ -41,14 +61,25 @@ sub parse_wheres {
             $option_key, $attribute_key, $attribute_value
           );
 
-          push(@{$wheres}, $query_string) if (length($query_string) > 0);
+          push(@{$where_ref}, $query_string) if (length($query_string) > 0);
         }
       }
     } else {
       my $query_string = build_where_query("eq", $key, $ref);
-      push(@{$wheres}, $query_string);
+      push(@{$where_ref}, $query_string);
     }
   }
+}
+
+sub parse_order {
+  my $order_ref = shift;
+  my %order_options = %{shift @_};
+  my @keys = keys %order_options;
+
+  my $direction = uc(shift(@keys));
+  my @columns = @{$order_options{lc $direction}};
+  my $joined_columns = join(", ", @columns);
+  $$order_ref = "ORDER BY $joined_columns $direction";
 }
 
 sub build_where_query {
--- t/Api/query_test.pl
+++ t/Api/query_test.pl
@@ -1,6 +1,6 @@
 use lib 'lib';
 use Api::Query;
-use Test::Simple tests => 2;
+use Test::Simple tests => 4;
 
 do {
   $expected = "SELECT * FROM example WHERE email = 'john\@example.com' AND id > '5' AND name LIKE 'john'";
@@ -22,4 +22,27 @@ do {
   $returned = Api::Query->build("example", {});
 
   ok($expected eq $returned, 'generates simple query when no options');
+};
+
+do {
+  $expected = "SELECT * FROM example WHERE name LIKE '\%john\%' ORDER BY id DESC";
+  $returned = Api::Query->build("example", {
+    order_by => {
+      desc => ["id"]
+    },
+    where => {
+      like => { name => '%john%' }
+    }
+  });
+
+  ok($expected eq $returned, 'generates a query with ORDER');
+};
+
+do {
+  $expected = "SELECT id, name FROM example";
+  $returned = Api::Query->build("example", {
+    columns => ["id", "name"]
+  });
+
+  ok($expected eq $returned, 'generates a query with custom columns');
 };
